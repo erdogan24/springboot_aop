5a073a5ed0575313901ea3d7b0ad284edfa57913
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-10-04 23:57:08 +0300

    Commit 4

--- src/main/java/com/code/aopdemo/dao/AccountDAOImpl.java
+++ src/main/java/com/code/aopdemo/dao/AccountDAOImpl.java
@@ -9,7 +9,7 @@ public class AccountDAOImpl implements AccountDAO{
 
     @Override
     public void addAccount() {
-        System.out.println(getClass() +": DOING MY DB WORK: ADDİNG AN ACCOUNT");
+        System.out.println(getClass() +": DOING MY DB WORK: ADDING AN ACCOUNT");
 
     }
 }
