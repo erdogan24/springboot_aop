b0ba0d45aa5e81f414dbdae62144e04432815b8a
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-10-04 23:26:21 +0300

    Commit

--- src/main/java/com/code/aopdemo/dao/AccountDAO.java
+++ src/main/java/com/code/aopdemo/dao/AccountDAO.java
@@ -2,4 +2,5 @@ package com.code.aopdemo.dao;
 
 public interface AccountDAO {
 
+    void addAccount();
 }
---
+++ src/main/java/com/code/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,10 @@
+package com.code.aopdemo.dao;
+
+public class AccountDAOImpl implements AccountDAO{
+
+
+    @Override
+    public void addAccount() {
+
+    }
+}
