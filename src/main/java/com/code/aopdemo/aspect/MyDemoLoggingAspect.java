d363d55edebc9a2718bfc16c447b8df15e8086f3
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-10-06 10:31:23 +0300

    commit 2

--- src/main/java/com/code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,7 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    @Before("execution(* add*(com.code.aopdemo.Account))")
+    @Before("execution(* com.code.aopdemo.dao.*.*(..))")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
