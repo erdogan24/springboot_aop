e9d3d1a62024bf45a8c42e1482b76eea3348bc4d
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-10-06 10:37:38 +0300

    commit 3

--- src/main/java/com/code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,7 +13,10 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    @Before("execution(* com.code.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* com.code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
