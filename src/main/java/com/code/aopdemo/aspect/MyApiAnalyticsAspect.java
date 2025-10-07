acf6c56919d0b3a746214e5d68e8f4d6c067d2d9
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-10-07 11:01:58 +0300

    commit 1

---
+++ src/main/java/com/code/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,12 @@
+package com.code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class MyApiAnalyticsAspect {
+
+
+}
